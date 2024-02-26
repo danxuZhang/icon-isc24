3664ce6a432ec355a29ec95709921a392aa9e15a
Panos Adamidis <adamidis@dkrz.de>
2024-02-26 16:58:11 +0100

    adding BSD-3 License Text to source files

--- core/common/utils.hpp
+++ core/common/utils.hpp
@@ -1,3 +1,14 @@
+// ICON
+//
+// ---------------------------------------------------------------
+// Copyright (C) 2004-2024, DWD, MPI-M, DKRZ, KIT, ETH, MeteoSwiss
+// Contact information: icon-model.org
+//
+// See AUTHORS.TXT for a list of authors
+// See LICENSES/ for license information
+// SPDX-License-Identifier: BSD-3-Clause
+// ---------------------------------------------------------------
+//
 #pragma once
 #include "types.hpp"
 
