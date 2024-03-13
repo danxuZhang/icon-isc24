0b546940b2e4fcc463b57fcc4ec24b824439424d
dxZhang <dzhang022@e.ntu.edu.sg>
2024-03-13 16:20:48 +0100

    Update build and run script

--- scripts/build-nvhpc-acc.sh
+++ scripts/build-nvhpc-acc.sh
@@ -7,19 +7,20 @@ spack load netcdf-cxx4@4.3.1
 
 export LD_LIBRARY_PATH=/sw/spack-levante/gcc-11.2.0-bcn7mb/lib64:$LD_LIBRARY_PATH
 
-
-# export TARGET="host" 
-export TARGET="multicore" 
-# export TARGET="gpu" 
+# export TARGET="host"
+# export TARGET="multicore"
+export TARGET="gpu"
 
 export CC=nvc
 export CXX=nvc++
-export CXXFLAGS="-std=c++17 -Minfo=all -acc=$TARGET"
+export CXXFLAGS="-std=c++17 -Minfo=accel -acc=$TARGET -gpu=managed"
 
 export PREFIX="build-nvhpc-acc"
 
-#configure muphys-cpp
-cmake -DMU_IMPL=openacc -B $PREFIX -S .
+rm -rf $PREFIX
 
-#build 
+#configure muphys-cpp
+cmake -DMU_IMPL=openacc -DMU_ENABLE_TESTS=OFF -B $PREFIX -S .
+
+#build
 cmake --build $PREFIX --parallel
