df94d6fc5adc735225e9014313ca4cafea284f7f
dxZhang <dzhang022@e.ntu.edu.sg>
2024-03-17 15:58:56 +0100

    Fix gcc link issue

--- scripts/build-gcc-acc.sh
+++ scripts/build-gcc-acc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-eval $(/work/k20200/k202174/spack/bin/spack load --sh gcc@12.3.0)
+module purge
+module load gcc/.12.3.0-gcc-11.2.0-nvptx
+export LD_LIBRARY_PATH=/sw/spack-levante/gcc-12.3.0-ab6j4u/lib64:$LD_LIBRARY_PATH
+# module load gcc/.13.2.0-gcc-11.2.0-nvptx
 
 spack load netcdf-cxx4@4.3.1
 
 export CC=gcc
 export CXX=g++
-export CXXFLAGS="-std=c++17 -fopenacc -foffload=nvptx-none"
+export CXXFLAGS="-std=c++17 -fopenacc -lm -foffload=nvptx-none -foffload-options=-lm"
 
 export PREFIX="build-gcc-acc"
 
